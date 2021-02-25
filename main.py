8a289536e0788ae0451ba427408bd9e7210a498c
Lemon Nyzer <lemonyzer@gmail.com>
2021-02-25 18:43:36 +0100

    exports lua Scripts by Group

--- main.py
+++ main.py
@@ -14,10 +14,35 @@ from pathlib import Path, PureWindowsPath
 from xml.dom import minidom
 
 
+# Example
+#<panel>
+# <luaManager>
+#  <luaManagerMethods>
+#   <luaMethodGroup name="Efx" uuid="4c6c3d448aae40b1affc1ef084551d80">
+#     <luaMethod luaMethodName="setModPresetEfx" luaMethodCode="function sendSysExMessage(msgData)&#13;&#10;&#13;&#10;&#9;--xx = tonumber(fxModeValue,16)&#13;&#10;&#13;&#10;&#9;--XX = 0x00 -- don't care&#13;&#10;&#13;&#10;&#9;--msgData = { 0xF0, 0x41, 0x00, 0x42, 0x12, 0x40, combinedByte, 0x22, 0x00, XX, 0xF7 }&#9;&#9;--&lt;-- Reset: track in normal mode&#13;&#10;&#13;&#10;&#9;console (string.format(&quot;sendSysExMessage (msgData=%s)&quot;, MemoryBlock(msgData):toHexString(1)))&#13;&#10;&#13;&#10;--&#9;for k,v in pairs(msgData) do console(string.format(&quot;SysEx: %d,%02x&quot;, k,v)) end&#13;&#10;&#9;--console ( tostring(msgData) )&#13;&#10;&#13;&#10;&#9;newMidiMessage = CtrlrMidiMessage( msgData )&#13;&#10;&#9;panel:sendMidiMessageNow(newMidiMessage)&#13;&#10;&#10;end"
+#                luaMethodLinkedProperty="" luaMethodSource="0" uuid="211c6dcc7ad0445ca86ffac96981e513"
+#                luaMethodValid="1"/>
+#   </luaMethodGroup>
+#   <luaMethodGroup name="MIDI" uuid="4c6c3d448aae40b1affc1ef084551d80">
+#     <luaMethod luaMethodName="sendSysExMessage" luaMethodCode="function sendSysExMessage(msgData)&#13;&#10;&#13;&#10;&#9;--xx = tonumber(fxModeValue,16)&#13;&#10;&#13;&#10;&#9;--XX = 0x00 -- don't care&#13;&#10;&#13;&#10;&#9;--msgData = { 0xF0, 0x41, 0x00, 0x42, 0x12, 0x40, combinedByte, 0x22, 0x00, XX, 0xF7 }&#9;&#9;--&lt;-- Reset: track in normal mode&#13;&#10;&#13;&#10;&#9;console (string.format(&quot;sendSysExMessage (msgData=%s)&quot;, MemoryBlock(msgData):toHexString(1)))&#13;&#10;&#13;&#10;--&#9;for k,v in pairs(msgData) do console(string.format(&quot;SysEx: %d,%02x&quot;, k,v)) end&#13;&#10;&#9;--console ( tostring(msgData) )&#13;&#10;&#13;&#10;&#9;newMidiMessage = CtrlrMidiMessage( msgData )&#13;&#10;&#9;panel:sendMidiMessageNow(newMidiMessage)&#13;&#10;&#10;end"
+#                luaMethodLinkedProperty="" luaMethodSource="0" uuid="211c6dcc7ad0445ca86ffac96981e513"
+#                luaMethodValid="1"/>
+#   </luaMethodGroup>
+
+# luaMethodGroups (parentNode)
+# |
+# |->luaMethod (childNode)
+#    |.Attribut['luaMethodCode']
+
+# itemlist = xmldoc.getElementsByTagName('luaMethod')
+# for s in itemlist:
+#     print(s.attributes['luaMethodName'].value)
+
 def createMethodScriptFile(path, lua_method):
     lua_method_name = lua_method.attributes['luaMethodName'].value
     full_path = PureWindowsPath(path).joinpath(lua_method_name+".lua")
 
+    #lua_method_code = lua_method.attributes['luaMethodCode'].value
     lua_method_code = lua_method.attributes['luaMethodCode'].value
     
     file = open(full_path, "w")
@@ -46,7 +71,8 @@ def createFoldersAndScriptFiles(root_folder, xmldoc):
         p = Path(PureWindowsPath(root_folder).joinpath(lua_method_group.attributes['name'].value))
         try:
             p.mkdir(parents=True)
-            lua_methods = xmldoc.getElementsByTagName('luaMethod')
+            lua_methods = lua_method_group.getElementsByTagName('luaMethod')
+            #lua_methods = lua_method_group.childNodes
             print(f"creating {len(lua_methods)} lua script files")
             for m in lua_methods:
                 createMethodScriptFile(p, m)
@@ -63,29 +89,6 @@ def main():
     # createFoldersOnly(panel_filename.split(".")[0],_lua_method_groups)
     createFoldersAndScriptFiles(panel_filename.split(".")[0], xmldoc)
 
-    
-
-
-
-
-    # itemlist = xmldoc.getElementsByTagName('luaMethod')
-    # print(len(itemlist))
-
-    # for s in itemlist:
-    #     print(s.attributes['luaMethodName'].value)
-
-    # print(itemlist[0].attributes['luaMethodCode'].value)
-    # for s in itemlist:
-    #     print(s.attributes['luaMethodCode'].value)
-
-
-    # for 
-
-    # file = open("testfile.txt", "w")
-
-    # file.write()
-
-    # file.close()
 
 
 if __name__ == "__main__":
